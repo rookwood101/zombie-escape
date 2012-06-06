8304c02e34a0c90a8408c8432a5ccfc11474a89f
Samuel Maddock <samuel.maddock@gmail.com>
2012-06-05 20:32:38 -0400

    Additional 5 sec added to min/max infection times

--- ZombieEscape/gamemode/sv_rounds.lua
+++ ZombieEscape/gamemode/sv_rounds.lua
@@ -1,5 +1,5 @@
-GM.CVars.ZSpawnMin			= CreateConVar( "ze_ztimer_min", 10, {FCVAR_REPLICATED}, "Minimum time from the start of the round until picking the mother zombie(s)." )
-GM.CVars.ZSpawnMax 			= CreateConVar( "ze_ztimer_max", 25, {FCVAR_REPLICATED}, "Maximum time from the start of the round until picking the mother zombie(s)." )
+GM.CVars.ZSpawnMin			= CreateConVar( "ze_ztimer_min", 15, {FCVAR_REPLICATED}, "Minimum time from the start of the round until picking the mother zombie(s)." )
+GM.CVars.ZSpawnMax 			= CreateConVar( "ze_ztimer_max", 30, {FCVAR_REPLICATED}, "Maximum time from the start of the round until picking the mother zombie(s)." )
 
 GM.CVars.MaxRounds			= CreateConVar( "ze_max_rounds", 8, {FCVAR_REPLICATED}, "Maximum amount of rounds played prior to map switch" )
 
